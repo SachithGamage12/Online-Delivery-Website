61cff53fd18450822682a92e79d989936718ff97
Sachith Gamage <sachithgamage2310@gmail.com>
2026-03-01 13:25:27 +0530

    Update db_connection.php

--- config/db_connection.php
+++ config/db_connection.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $dbname = 'lakway_delivery';
 $username = 'root';
-$password = 'Sun123flower@';
+$password = '';//use your password 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
